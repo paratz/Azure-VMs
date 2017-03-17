2ecf6e8f6506d272c520e9493b91d0f7bf957e63
paratz <pablo@ratz.com.ar>
2017-03-17 15:30:10 -0300

    upload
    upload

---
+++ Create-AzureVMsLab.ps1
@@ -0,0 +1,175 @@
+﻿#region begin Inicio de Sesión 
+
+Login-AzureRmAccount
+
+Get-AzureRmSubscription
+
+Select-AzureRmSubscription -SubscriptionId <IDDelaSubscripción>
+
+#endregion
+
+#region begin Crear Resource Group
+#Verificar las Ubicaciones disponibles en la subscripción
+
+Get-AzureRmLocation | sort Location | Select Location
+
+$location = "brazilsouth"
+
+#Crear Resource Group
+
+$myResourceGroup = "Contoso-LAB-RG"
+
+New-AzureRmResourceGroup -Name $myResourceGroup -Location $location
+
+#endregion
+
+#region begin Crear Cuenta de Storage
+
+$myStorageAccountName = "Contoso-LAB-StorageAccount"
+Get-AzureRmStorageAccountNameAvailability $myStorageAccountName
+
+$myStorageAccount = New-AzureRmStorageAccount -ResourceGroupName $myResourceGroup -Name $myStorageAccountName -SkuName "Standard_LRS" -Kind "Storage" -Location $location
+
+#Crear el Contenedor
+New-AzureStorageContainer -name VHDs -Permission Off -Context $myStorageAccount
+
+#endregion 
+
+# Detener VM DT-DC
+
+#region begin Copiar VHDs de OS y Datos de una Cuenta a la nueva cuenta
+
+#################################################
+#Copiar OS Disk
+#################################################
+
+# Source VHD
+$srcVhd = "https://contosostoragebrasil.blob.core.windows.net/vhds/contoso-DC-DTDCAzureBrasil-2015-05-08.vhd"
+ 
+# Destination VHD name
+ 
+$destVhdName = "contoso-LAB-DCAZUREBRASIL-OS.vhd"
+ 
+# Destination Container Name 
+$destContainerName = "vhds"
+ 
+# Source Storage Account and Key
+ 
+$srcStorageAccount = "dtstoragebrasil"
+$srcStorageKey = "<storageaccountkey>"
+ 
+# Target Storage Account and Key
+ 
+$destStorageAccount = $myStorageAccountName
+$destStorageKey = "<storageaccountkey>"
+ 
+# Create the source storage account context (this creates the context, it does not actually create a new storage account)
+ 
+$srcContext = New-AzureStorageContext –StorageAccountName $srcStorageAccount –StorageAccountKey $srcStorageKey
+                                         
+ 
+# Create the destination storage account context 
+ 
+$destContext = New-AzureStorageContext –StorageAccountName $destStorageAccount –StorageAccountKey $destStorageKey
+
+ 
+# Start the copy  
+ 
+$blob1 = Start-AzureStorageBlobCopy -Context $srcContext –AbsoluteUri $srcVhd –DestContainer $destContainerName –DestBlob $destVhdName –DestContext $destContext -Verbose 
+ 
+ 
+# check status of copy
+ 
+$blob1 | Get-AzureStorageBlobCopyState
+
+#################################################
+#Copiar DATA Disk
+#################################################
+
+# Source VHD
+$srcdataVhd = "https://contosostoragebrasil.blob.core.windows.net/vhds/DT-DC-Azure-DTDCAzureBrasil-0508-1.vhd"
+ 
+# Destination VHD name
+ 
+$destdataVhdName = "contoso-LAB-DCAZUREBRASIL-DATA.vhd"
+ 
+# Destination Container Name 
+$destContainerName = "vhds"
+ 
+# Create the source storage account context (this creates the context, it does not actually create a new storage account)
+ 
+$srcContext = New-AzureStorageContext –StorageAccountName $srcStorageAccount –StorageAccountKey $srcStorageKey
+                                         
+ 
+# Create the destination storage account context 
+ 
+$destContext = New-AzureStorageContext –StorageAccountName $destStorageAccount –StorageAccountKey $destStorageKey
+
+ 
+# Start the copy  
+ 
+$blob2 = Start-AzureStorageBlobCopy -Context $srcContext –AbsoluteUri $srcdataVhd –DestContainer $destContainerName –DestBlob $destdataVhdName –DestContext $destContext -Verbose 
+
+ 
+# check status of copy
+ 
+$blob2 | Get-AzureStorageBlobCopyState
+
+#endregion
+
+#region begin Crear Nueva Maquina Virtual
+
+########################################################################
+ 
+#Create a Virtual Machine from an existing OS disk
+ 
+########################################################################
+ 
+
+$vmName= "contoso-LAB-DTDCAZUREBRASIL"
+$vmSize="Standard_A1"
+
+$myPublicIpName = "contoso-LAB-DTDCAZUREBRASIL-PublicIP"
+$vnetName= "contoso-LAB-VNET1"
+$SubnetName = "contoso-LAB-VNET1-SUBNET1"
+$nicName = "contoso-LAB-DTDCAZUREBRASIL-NIC1"
+ 
+# Get storage account configuration for the target storage account
+ 
+$StorageAccount = Get-AzureRmStorageAccount –ResourceGroupName $myResourceGroup –AccountName $myStorageAccountName
+  
+#Create and Get Virtual Network configuration
+
+$myPublicIp = New-AzureRmPublicIpAddress -Name $myPublicIpName -ResourceGroupName $myResourceGroup -Location $location -AllocationMethod Dynamic
+
+$mySubnet = New-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix 192.168.0.0/24
+
+New-AzureRmVirtualNetwork -ResourceGroupName $myResourceGroup -Name $vnetName -AddressPrefix 192.168.0.0/16 -Location $location -Subnet $mysubnet
+
+$vnet = Get-AzureRmVirtualNetwork -Name $vnetName –ResourceGroupName $myResourceGroup 
+ 
+# Create VM from an existing image
+
+$vm = New-AzureRmVMConfig –vmName $vmName –vmSize $vmSize
+  
+$nic = New-AzureRmNetworkInterface -Name $nicName –ResourceGroupName $myResourceGroup -Location $location –SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $myPublicIp.Id 
+
+$vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $nic.Id 
+
+#Conseguir la URI del disco copiado
+
+$StorageAccountContext = New-AzureStorageContext –StorageAccountName $myStorageAccountName –StorageAccountKey $myStorageAccountKey
+
+$destOSDiskUri = (Get-AzureStorageBlob -blob $destVhdName -Container $destContainerName -Context $StorageAccountContext ).ICloudBlob.uri.AbsoluteUri 
+    
+# Set the OS disk properties for the new VM. If you are migrating a Linux machine use the -Linux switch instead of -Windows
+
+$vm = Set-AzureRmVMOSDisk -VM $vm -Name $destVhdName –VhdUri $destOSDiskUri -Windows –CreateOption attach
+
+$DataDiskUri = (Get-AzureStorageBlob -blob $destdataVhdName -Container $destContainerName -Context $StorageAccountContext ).ICloudBlob.uri.AbsoluteUri 
+
+$vm = Add-AzureRmVMDataDisk -VM $vm -Name "DT-LAB-DTDCAZUREBRASIL-DATADISK1" -VhdUri $DataDiskUri -Lun 0 -DiskSizeinGB 5 -CreateOption Attach
+
+New-AzureRmVM –ResourceGroupName $myresourcegroup -Location $location -VM $vm
+
+#endregion 
---
+++ OtherSetup-AzureLab.ps1
@@ -0,0 +1,157 @@
+﻿#region Definición de Variables
+
+$SubscriptionID = "5ab28d9e-f464-45d8-adeb-c7a0e8c678f6"
+
+$ResourceGroupName = "PLRLab"
+$RGLocation = "brazilsouth"
+# List of available regions is 'centralus,eastasia,southeastasia,eastus,eastus2,westus,westus2,northcentralus,
+# southcentralus,westcentralus,northeurope,westeurope,japaneast,japanwest,brazilsouth,australiasoutheast,
+# australiaeast,westindia,southindia,centralindia,canadacentral,canadaeast,uksouth,ukwest,koreacentral,koreasouth'
+
+$StorageAccountType = "Standard_LRS"
+#"Standard_LRS,Standard_ZRS,Standard_GRS,Standard_RAGRS,Premium_LRS"
+
+$1stVMLocalPathOSDisk = "C:\Users\Arwen\Documents\Virtual Machines\DC1ConvertedDisk\DC1-OS.vhd"
+$1stVMOSDiskName = "DC1OS.vhd"
+
+$1stVMLocalPathVMDATADisk = "C:\Users\Arwen\Documents\Virtual Machines\DC1ConvertedDisk\DC1-DATA.vhd"
+$1stVMDATADiskName = "DC1DATA.vhd"
+
+$vnetprefix = "10.0.0.0/16"
+$subnetprefix = "10.0.0.0/24"
+$PrivateIPAddress = "10.0.0.11"
+
+$1stDNSServer = $PrivateIPAddress
+$2ndDNSServer = "10.0.0.12"
+
+$VMName = "DC01"
+$VMSize = "Standard_A2_v2"
+
+#Para ver la lista de tamaños disponibles en la versión elegida ejecutar:
+#Get-AzureRmVmSize -Location $RGLocation | Sort-Object Name | ft Name, NumberOfCores, MemoryInMB, MaxDataDiskCount -AutoSize
+
+#endregion
+
+#region Inicio de Sesión 
+
+#Instalar modulo de Powershell de Azure en caso de que no esté instalado
+Install-Module AzureRM.Compute -RequiredVersion 2.6.0
+
+Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+
+#endregion
+
+#region Creación de grupo de recursos y cuenta de almacenamiento
+
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $RGLocation 
+
+$StorageAccountName = $ResourceGroupName + "Storage"
+
+New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -AccountName $StorageAccountName.ToLower() -Location $RGLocation -Type $StorageAccountType
+
+#endregion
+
+#region Subir Discos OS y Datos Maquina 1
+
+$StorageAccountContainer = "vhds"
+$urlOfUploadedImageOSVhd = "https://" + $StorageAccountName.ToLower() + ".blob.core.windows.net/" + $StorageAccountContainer + "/" + $1stVMOSDiskName
+
+Add-AzureRmVhd -ResourceGroupName $ResourceGroupName -Destination $urlOfUploadedImageOSVhd -LocalFilePath $1stVMLocalPathOSDisk
+
+$urlOfUploadedImageDATAVhd = "https://" + $StorageAccountName.ToLower() + ".blob.core.windows.net/" + $StorageAccountContainer + "/" + $1stVMDATADiskName
+
+Add-AzureRmVhd -ResourceGroupName $ResourceGroupName -Destination $urlOfUploadedImageDATAVhd -LocalFilePath $1stVMLocalPathVMDATADisk
+
+#endregion
+
+#region crear red virtual, subnet, public ip, nic y ngs
+
+$vnetName = $ResourceGroupName + "-VNET"
+$subnetName = $vnetName + "-Subnet01"
+
+$singleSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetprefix
+$vnet = New-AzureRmVirtualNetwork -Name $vnetName -ResourceGroupName $ResourceGroupName -Location $RGLocation -AddressPrefix $vnetprefix -Subnet $singleSubnet
+
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroupName -name $vnetName 
+$vnet.DhcpOptions.DnsServers = $1stDNSServer
+$vnet.DhcpOptions.DnsServers += $2ndDNSServer 
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
+
+
+$PublicIPName = $vmname + "-PublicIP"
+
+$pip = New-AzureRmPublicIpAddress -Name $PublicIPName -ResourceGroupName $ResourceGroupName -Location $RGLocation `
+     -AllocationMethod Dynamic
+
+$nicName = $VMName + "-NIC1"
+
+$nic = New-AzureRmNetworkInterface -Name $nicName -ResourceGroupName $ResourceGroupName `
+ -Location $RGLocation -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -PrivateIpAddress $PrivateIPAddress
+
+$nsgName = $VMName + "-NetworkSecurityGroup"
+$RdpruleName = $VMName + "-RDPRule"
+
+$rdpRule = New-AzureRmNetworkSecurityRuleConfig -Name $RdpruleName -Description "Allow RDP" `
+    -Access Allow -Protocol Tcp -Direction Inbound -Priority 110 `
+    -SourceAddressPrefix Internet -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange 3389
+
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Location $RGLocation `
+    -Name $nsgName -SecurityRules $rdpRule
+
+#endregion
+
+#region configurar VM
+
+$vmConfig = New-AzureRmVMConfig -VMName $vmname -VMSize $VMSize
+
+$vm = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $nic.Id
+
+$OSDiskName = $VMName + "-OSDisk"
+$urlOfUploadedImageOSVhd = "https://" + $StorageAccountName.ToLower() + ".blob.core.windows.net/" + $StorageAccountContainer + "/" + $1stVMOSDiskName
+
+#Crear el Disco utilizando Managed Disk (no funiciona en este momento)
+#$osDisk = New-AzureRmDisk -DiskName $OSDiskName -Disk (New-AzureRmDiskConfig `
+#-AccountType $StorageAccountType  -Location $RGLocation -CreationDataCreateOption Import `
+#-SourceUri $urlOfUploadedImageOSVhd ) `
+#-ResourceGroupName $ResourceGroupName
+#
+# $vm = Set-AzureRmVMOSDisk -VM $vm -ManagedDiskId $osDisk.Id -ManagedDiskStorageAccountType $StorageAccountType `
+# -DiskSizeInGB 128 -CreateOption Attach -Windows
+
+$vm = Set-AzureRmVMOSDisk -VM $vm -Name $OSDiskName -VhdUri $urlOfUploadedImageOSVhd -CreateOption attach -Windows
+
+$DataDiskName = $VMName + "-DATADisk"
+$urlOfUploadedImageDATAVhd = "https://" + $StorageAccountName.ToLower() + ".blob.core.windows.net/" + $StorageAccountContainer + "/" + $1stVMDATADiskName
+
+#Crear el Disco utilizando Managed Disk (no funiciona en este momento)
+#$DataDisk = New-AzureRmDisk -DiskName $DataDiskName -Disk (New-AzureRmDiskConfig ` 
+# -AccountType $StorageAccountType -Location $RGLocation -CreateOption Import `
+# -SourceUri $urlOfUploadedImageDATAVhd) `
+# -ResourceGroupName $ResourceGroupName
+#
+#$vm = Add-AzureRmVMDataDisk -VM $vm -Name $dataDiskName -CreateOption Attach -ManagedDiskId $DataDisk.Id -Lun 1
+
+$DataDiskSize = 10
+$vm = Add-AzureRmVMDataDisk -VM $vm -Name $DataDiskName -VhdUri $urlOfUploadedImageDATAVhd -Lun 1 -CreateOption attach -DiskSizeInGB $DataDiskSize
+
+#endregion
+
+#region Crear VM
+
+New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $RGlocation -VM $vm
+
+#endregion
+
+#region links adicionales
+#
+# https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-create-vm-specialized?toc=%2fazure%2fvirtual-machines%2fwindows%2ftoc.json
+# https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-create-managed-disk-ps
+# https://docs.microsoft.com/en-us/azure/virtual-network/virtual-networks-static-private-ip-arm-ps
+# https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-sizes
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-common-deployment-errors#skunotavailable
+# https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-ps-create
+# 
+#endregion
