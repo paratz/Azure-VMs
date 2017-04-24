2a6a6992fa4b405372af7510a87c9a5e87410e87
paratz <pablo@ratz.com.ar>
2017-04-24 16:13:05 -0300

    v 1.1
    forgot .xml extension in export file

--- Export-AllClassicVMs.ps1
+++ Export-AllClassicVMs.ps1
@@ -1,5 +1,5 @@
 ﻿$VMs = Get-AzureVM 
 foreach ($v in $VMs) { 
-    $File = "C:\tmp\" + $v.Name
+    $File = "C:\tmp\" + $v.Name + ".xml"
     Export-AzureVM -ServiceName $v.ServiceName -Name $v.Name -Path $File
 }
