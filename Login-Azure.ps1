f586c5cb5c42bce5638d8d6a8db8ec9144068c02
paratz <pablo@ratz.com.ar>
2017-04-25 15:31:51 -0300

    v1

---
+++ Login-Azure.ps1
@@ -0,0 +1,17 @@
+﻿#SubscriptionName                 SubscriptionId                      
+#----------------                 --------------                      
+#Visual Studio Enterprise         xxxxxxxxxe-f464-45d8-adeb-c7a0e8c678f6
+#Visual Studio Ultimate with MSDN xxxxxxxx-c5a1-4bee-8f4a-5ccaeccc0787
+
+Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionId cfcb919c-c5a1-4bee-8f4a-5ccaeccc0787
+
+#Iniciar Sesión en Portal Clásico
+Add-AzureAccount
+
+#Listar todas las subscripciones existentes
+Get-AzureSubscription
+
+#Seleccionar la subscripción donde se trabajará (reemplazar el subscriptionID obtenido del comando anterior)
+Select-AzureSubscription -SubscriptionId cfxxxxxxxxc-c5a1-4bee-8f4a-5ccaeccc0787
