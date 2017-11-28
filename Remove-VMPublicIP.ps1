e1bedc24d437f7dddfcd6073d75d2594e055d75a
paratz <pablo@ratz.com.ar>
2017-11-28 10:58:54 -0300

    v1 - sebastian
    v1

---
+++ Remove-VMPublicIP.ps1
@@ -0,0 +1,19 @@
+﻿#Script create by sebastid
+
+Login-AzureRmAccount
+
+$vms =Get-AzureRmVM -ResourceGroupName fabrikam.com.ar -Name adtrusttest
+
+$nics = $vms.networkprofile
+
+# on this foreach we take the networkprofile and then we are using the split property so we can parse the name of the NIC, right after that
+# we are modifying the ipconfiguration.publicipaddress to "" that means none
+foreach ($vm in $vms)
+{
+$nics = $vm.networkprofile
+$nicname = $nics.networkinterfaces.id.split("/")[8]
+$nicobject = Get-AzureRmNetworkInterface -Name $nicname -ResourceGroupName $vm.resourcegroupname
+
+$nicobject.IpConfigurations.PublicIpAddress.Id = ""
+$nicobject | Set-AzureRmNetworkInterface
+}
