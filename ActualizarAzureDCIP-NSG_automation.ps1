e1c07c55bae1d28d8f68459e822cd39ca4956642
paratz <pablo@ratz.com.ar>
2018-03-27 16:08:09 -0300

    2.0
    soporte para multiples datacenters en el mismo script

--- ActualizarAzureDCIP-NSG_automation.ps1
+++ ActualizarAzureDCIP-NSG_automation.ps1
@@ -1,8 +1,16 @@
-﻿$NSGName = "fabwap1-nsg"
-$OutboundRuleName = "Azure_WestUS"
-$RGName = "fabrikam.com.ar"
+﻿$Rules = @(
+    @{RuleName="Azure_DataCenter_UScentral";Location="uscentral"},
+    @{RuleName="Azure_DataCenter_USnorth";Location="usnorth"},
+    @{RuleName="Azure_DataCenter_USwestcentral";Location="uswestcentral"},
+    @{RuleName="Azure_DataCenter_USwest2";Location="uswest2"},
+    @{RuleName="Azure_DataCenter_USwest";Location="uswest"},
+    @{RuleName="Azure_DataCenter_USsouth";Location="ussouth"},
+    @{RuleName="Azure_DataCenter_EastUS";Location="eastus"},
+    @{RuleName="Azure_DataCenter_EastUS2";Location="eastus2"}) | % { New-Object object | Add-Member -NotePropertyMembers $_ -PassThru }
+
 $SubscriptionID = "cfcb919c-c5a1-4bee-8f4a-5ccaeccc0787"
-$region = "uswest"
+
+#This script will search all the NSG in a subscription and will update the required datacenter rule with the IP addresses from the xml file
 
 $connectionName = "AzureRunAsConnection"
 try
@@ -31,46 +39,66 @@ catch {
 Select-AzureRmSubscription -SubscriptionId $SubscriptionID
 
 
-# Invocar función que trae el contenido del xml con las IPs de Datacenter (https://buildwindows.wordpress.com/2017/11/19/get-azure-datacenter-ip-ranges-via-api/)
+#Recorro todos los network security groups y si hay alguna regla de salida que coincide con el nombre definido al principio del script, actualiza la regla.
 
-$body = @{“region”=“uswest”;“request”=“dcip”} | ConvertTo-Json
+$nsgs = Get-AzureRmNetworkSecurityGroup
 
-$webrequest = Invoke-WebRequest -Method “POST” -uri `
-https://azuredcip.azurewebsites.net/api/azuredcipranges -Body $body
+foreach ($nsg in $nsgs) {
 
-ConvertFrom-Json -InputObject $webrequest.Content 
+    foreach ($secrule in $nsg.SecurityRules) {
+        
+        foreach ($DCrule in $Rules) {
+            
+            $OutboundRuleName = $DCRule.RuleName
+            $region = $DCrule.Location
+                              
+            if ($secrule.Name -eq $OutboundRuleName) {
 
-$IPs = $webrequest.Content
+            # Invocar función que trae el contenido del xml con las IPs de Datacenter (https://buildwindows.wordpress.com/2017/11/19/get-azure-datacenter-ip-ranges-via-api/)
 
-#Remuevo los ultimos dos caracteres
-$IPs = $IPs.Substring(0,$IPs.Length-2)
+            $body = @{“region”=“$region”;“request”=“dcip”} | ConvertTo-Json
 
-#Remuevo los primero caracteres ( el largo de la locación + 5 otros caracteres que están al principio)
-$IPs = $IPs.Substring($region.Length + 5)
+            $webrequest = Invoke-WebRequest -Method “POST” -uri `
+            https://azuredcip.azurewebsites.net/api/azuredcipranges -Body $body -UseBasicParsing
 
-#Remuevo las comillas
-$IPs = $IPs -replace '["]'
+            ConvertFrom-Json -InputObject $webrequest.Content 
 
-#Convertir string a array
-$ArrayIPs = $IPs.Split(",")
+            $IPs = $webrequest.Content
 
-#Convertir string a lista generica (el comando Set-AzureRmNetworkSecurityRuleConfig requiere este tipo de objeto para el parametro DestinationAddressPreffix)
-[Collections.Generic.List[String]]$ListaIPs = $ArrayIPs
+            #Remuevo los ultimos dos caracteres
+            $IPs = $IPs.Substring(0,$IPs.Length-2)
 
-# Modificar Regla
+            #Remuevo los primero caracteres ( el largo de la locación + 5 otros caracteres que están al principio)
+            $IPs = $IPs.Substring($region.Length + 5)
 
-$nsg = Get-AzureRmNetworkSecurityGroup -ResourceGroupName $RGName -Name $NSGName
+            #Remuevo las comillas
+            $IPs = $IPs -replace '["]'
 
-    Set-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsg `
-    -Name $OutboundRuleName `
-    -Description "Allow Access to Azure $($region) Datacenter" `
-    -Access Allow `
-    -Protocol Tcp `
-    -Direction Outbound `
-    -Priority 666 `
-    -SourceAddressPrefix VirtualNetwork `
-    -SourcePortRange * `
-    -DestinationAddressPrefix $ListaIPs `
-    -DestinationPortRange 443
+            #Convertir string a array
+            $ArrayIPs = $IPs.Split(",")
 
-    Set-AzureRmNetworkSecurityGroup -NetworkSecurityGroup $nsg
+            #Convertir string a lista generica (el comando Set-AzureRmNetworkSecurityRuleConfig requiere este tipo de objeto para el parametro DestinationAddressPreffix)
+            [Collections.Generic.List[String]]$ListaIPs = $ArrayIPs
+
+
+            $nsgamodificar = Get-AzureRmNetworkSecurityGroup -Name $nsg.Name -ResourceGroupName $nsg.ResourceGroupName
+        
+            Set-AzureRmNetworkSecurityRuleConfig -NetworkSecurityGroup $nsgamodificar `
+            -Name $OutboundRuleName `
+            -Description "Allow Access to Azure $($region) Datacenter" `
+            -Access Allow `
+            -Protocol Tcp `
+            -Direction Outbound `
+            -Priority $secrule.Priority `
+            -SourceAddressPrefix VirtualNetwork `
+            -SourcePortRange * `
+            -DestinationAddressPrefix $ListaIPs `
+            -DestinationPortRange 443
+
+            Set-AzureRmNetworkSecurityGroup -NetworkSecurityGroup $nsgamodificar
+
+            }
+        }
+
+    }
+}
---
+++ Export-NSGAndRules.ps1
@@ -0,0 +1,34 @@
+﻿Login-AzureRmAccount
+
+Select-AzureRmSubscription -Subscription 36c19581-28b3-41ad-ad0f-ba6c866e03b6
+
+$myarray = @()
+
+$NSGs = Get-AzureRmNetworkSecurityGroup
+
+foreach ($NSG in $NSGs){
+
+    foreach ($SecRule in $NSG.SecurityRules) {
+
+      $myobj = New-Object -TypeName PSObject
+
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'NSGName' -Value $NSG.Name
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'SecRuleName' -Value $SecRule.Name
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'Protocol' -Value $SecRule.Protocol
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'SourcePortRange' -Value $SecRule.SourcePortRange
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'DestinationPortRange' -Value $SecRule.DestinationPortRange
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'SourceAddressPrefix' -Value $SecRule.SourceAddressPrefix
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'DestinationAdressPrefix' -Value $SecRule.DestinationAddressPrefix
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'Access' -Value $SecRule.Access
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'Priority' -Value $SecRule.Priority
+      Add-Member -InputObject $myobj -MemberType 'NoteProperty' -Name 'Direction' -Value $SecRule.Direction 
+  
+      $myarray += $myobj
+
+    }
+ }
+
+ $myarray 
+
+
+
