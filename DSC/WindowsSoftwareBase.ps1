78daf15123bad60cee3a6c824e81484d5f493f54
Pablo Ratz <paratz@users.noreply.github.com>
2019-10-16 10:49:56 -0300

    a
    a

--- DSC/WindowsSoftwareBase.ps1
+++ DSC/WindowsSoftwareBase.ps1
@@ -16,7 +16,7 @@ Node localhost
     Package InstallNSClientv1
     {
         Ensure = "Present"
-        Name = "NSClientpp"
+        Name = "NSClient++ (x64)"
         Path = "https://paratz.blob.core.windows.net/pubparatz/NSClient++-Stable-64.msi"
         Arguments = "ALLOWED_HOSTS=192.168.1.1 NSCLIENT_PWD=Password123 CONF_CHECKS=1 CONF_NSCLIENT=1 CONF_NRPE=1 CONF_NSCA=1"
         ProductId = "07C65535-76EB-43F7-8D88-C841E1FA2F8A"
@@ -24,7 +24,7 @@ Node localhost
 
      Service NSClientServiceStart
         {
-            Name        = "nsclient++.exe"
+            Name        = "nsclientpp.exe"
             StartupType = "Automatic"
             State       = "Running"
         }
