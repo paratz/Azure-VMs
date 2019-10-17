61bef4a1d5383c9e04a7f0bf9104eddecc4445f3
Pablo Ratz <paratz@users.noreply.github.com>
2019-10-17 15:01:28 -0300

    algo

--- DSC/PowershellCommands.ps1
+++ DSC/PowershellCommands.ps1
@@ -1,1 +1,5 @@
-Import-AzureRmAutomationDscConfiguration -SourcePath 'C:\DscConfigs\TestConfig.ps1' -ResourceGroupName 'MyResourceGroup' -AutomationAccountName 'myAutomationAccount' -Published
+Select-AzSubscription -Subscription cbca3a93-a647-464b-b7de-8a3ab84c2a75
+
+Import-AzAutomationDscConfiguration -SourcePath 'C:\GitHub\Azure-VMs\DSC\LinuxSoftwareBase.ps1' -ResourceGroupName 'management-prod-rg'  -AutomationAccountName 'aa-prod-management' -Published
+
+Start-AzAutomationDscCompilationJob -ConfigurationName 'LinuxSoftwareBasev1' -ResourceGroupName 'management-prod-rg' -AutomationAccountName 'aa-prod-management'
