5aad677dd7bea30222e6f8c91093db8ff9e1a4ad
Pablo Ratz <paratz@users.noreply.github.com>
2019-12-11 09:14:08 -0300

    Update Write-ImmutableID.ps1

--- AzureAD/Write-ImmutableID.ps1
+++ AzureAD/Write-ImmutableID.ps1
@@ -3,8 +3,6 @@ if ($UserPrincipalName -eq $null) {
 $UserPrincipalName = read-host -Prompt "Por favor, Ingrese el UserPrincipalName" 
 }
 
-Start-Transcript
-
 #Si hay algun error, que detenga la ejecución
 $ErrorActionPreference = 'Stop'
 
@@ -13,17 +11,19 @@ $ErrorActionPreference = 'Stop'
 #Limpiamos la variable $ImmutableID de usos anteriores
 $ImmutableID = $Null
 
+#Verifica que el usuario cloud no tenga ImmutableID
+
+$ImmutableID = Get-MsolUser -UserPrincipalName $UserPrincipalName| Select-Object ImmutableID
+
 # Obtener Immutable ID en base a cuenta AD Onprem
 
 $ImmutableID = Get-ADUser -Filter "userprincipalname -eq '$UserPrincipalName'" -Properties ObjectGUID | select ObjectGUID | foreach {[system.convert]::ToBase64String(([GUID]($_.ObjectGUID)).tobytearray())}
 
-write-host "El ImmutableID del Usuario $UserPrincipalName es $ImmutableID"
+write-host -ForegroundColor Green "El ImmutableID del Usuario $UserPrincipalName es $ImmutableID"
 
 # Estampar en usuario Cloud el ImmutableID
-Write-Host "Se escribirá el Immutable ID $ImmutableID en el usuario cloud $UserPrincipalName"
+Write-Host -ForegroundColor Green "Se escribirá el Immutable ID $ImmutableID en el usuario cloud $UserPrincipalName"
 
 Set-MsolUser -UserPrincipalName $UserPrincipalName -ImmutableId $ImmutableID -Verbose
 
-Write-Host "Se escribió el Immutable ID $ImmutableID en el usuario cloud $UserPrincipalName"
-
-Stop-Transcript
+Write-Host -ForegroundColor Green "Se escribió el Immutable ID $ImmutableID en el usuario cloud $UserPrincipalName"
