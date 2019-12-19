f88ba44a4071f91b62566832e9d1bfbca11f4fea
Pablo Ratz <paratz@users.noreply.github.com>
2019-12-19 11:57:12 -0300

    fixe

--- DSC/LinuxSoftwareBase.ps1
+++ DSC/LinuxSoftwareBase.ps1
@@ -2,7 +2,6 @@
 {
 
     Import-DSCResource -Module nx
-    Import-DscResource -Module LinuxResourcesDSC
 
     Node localhost
     {
@@ -21,34 +20,31 @@
             PackageManager = "Yum"
             DependsOn = '[nxPackage]epel-release'
         }
-        
-        nxScript ConfiguraAllowedHosts {
 
-                GetScript = @"
-            #!/bin/bash
-            grep -c "allowed_hosts=127.0.0.1,::1" /etc/nagios/nrpe.cfg
-            "@
+            nxScript ConfiguraAllowedHosts {
 
-                SetScript = @"
-            #!/bin/bash
-            sed -i 's/allowed_hosts=127.0.0.1,::1/allowed_hosts=127.0.0.1,192.168.1.1,::1/' /etc/nagios/nrpe.cfg
-            "@
+    GetScript = @"
+#!/bin/bash
+grep -c "allowed_hosts=127.0.0.1,::1" /etc/nagios/nrpe.cfg
+"@
 
-                TestScript = @'
-            #!/bin/bash
-            linecount=`grep -c "allowed_hosts=127.0.0.1,::1" /etc/nagios/nrpe.cfg`
-            if [ $linecount -eq 1 ]
-            then
-                exit 1
-            else
-                exit 0
-            fi
-            '@
-                }
+    SetScript = @"
+#!/bin/bash
+sed -i 's/allowed_hosts=127.0.0.1,::1/allowed_hosts=127.0.0.1,192.168.1.1,::1/' /etc/nagios/nrpe.cfg
+"@
+
+    TestScript = @'
+#!/bin/bash
+linecount=`grep -c "allowed_hosts=127.0.0.1,::1" /etc/nagios/nrpe.cfg`
+if [ $linecount -eq 1 ]
+then
+    exit 1
+else
+    exit 0
+fi
+'@
+    }
         
 
     }
 }
-
-#config de ejemplo https://azurestack.blog/2016/02/adding-and-using-the-dsc-for-linux-vm-extension-to-azure-stack-tp1/
-#como instalar epel https://www.shellhacks.com/epel-repo-centos-7-6-install/
